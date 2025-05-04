addeb5f5ad9238ee97a954c96ad154d87ac91dde
Maulik Gajipara <55870961+gajiparamaulik@users.noreply.github.com>
2025-05-04 22:56:55 +0530

    added script js

---
+++ challenge-2/script.js
@@ -0,0 +1,19 @@
+const toggle = document.getElementById('darkModeToggle');
+const body = document.body;
+
+// Load saved mode from localStorage
+const darkMode = localStorage.getItem('dark-mode');
+if (darkMode === 'enabled') {
+	body.classList.add('dark');
+}
+
+toggle.addEventListener('click', () => {
+
+	body.classList.toggle('dark');
+	console.log("click function called");
+	if (body.classList.contains('dark')) {
+		localStorage.setItem('dark-mode', 'enabled');
+	} else {
+		localStorage.setItem('dark-mode', 'disabled');
+	}
+});
